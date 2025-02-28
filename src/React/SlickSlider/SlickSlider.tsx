eaa5e14d5e1c447b87021571905aeedcb4f3508f
kengomasunari2025 <kengomasunari2025@gmail.com>
2025-02-28 16:38:55 +0900

    slider変更

--- src/React/SlickSlider/SlickSlider.tsx
+++ src/React/SlickSlider/SlickSlider.tsx
@@ -2,29 +2,30 @@ import Cards from "./Card/Cards";
 
 const SlickSlider = () => {
   return (
-    <section className="py-5">
-      <h1 className="fw-bold text-center">制作実績</h1>
-      <div className="d-flex align-items-center justify-content-center w-90-parsent">
-        <span className="hover bg-dark text-white py-1 px-2 fw-bold slider-btn">＜</span>
-        <div
-          className="bg-white w-90-parsent"
-          style={{ overflowX: "auto", whiteSpace: "nowrap" }}
-        >
-          <ul id="ul" className="d-inline-flex">
-            {Cards.map((item, index) => (
-              <li
-                className="mx-2"
-                key={index}
-                style={{ display: "inline-block" }}
-              >
-                {item.card}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <span className="hover bg-dark text-white py-1 px-2 fw-bold slider-btn">＞</span>
+    <div className="d-flex align-items-center justify-content-center w-90-parsent">
+      <span className="hover bg-dark text-white py-1 px-2 fw-bold slider-btn">
+        ＜
+      </span>
+      <div
+        className="bg-white w-90-parsent py-5"
+        style={{ overflowX: "auto", whiteSpace: "nowrap" }}
+      >
+        <ul id="ul" className="d-inline-flex">
+          {Cards.map((item, index) => (
+            <li
+              className="mx-2"
+              key={index}
+              style={{ display: "inline-block" }}
+            >
+              {item.card}
+            </li>
+          ))}
+        </ul>
       </div>
-    </section>
+      <span className="hover bg-dark text-white py-1 px-2 fw-bold slider-btn">
+        ＞
+      </span>
+    </div>
   );
 };
 

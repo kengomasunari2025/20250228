ddc8ba167b852e30738fa4ff17f320fd7d79cbb0
kengomasunari2025 <kengomasunari2025@gmail.com>
2025-02-28 16:43:51 +0900

    l

--- src/React/Footer/Footer.tsx
+++ src/React/Footer/Footer.tsx
@@ -2,10 +2,6 @@
 const Footer = () => {
   return (
     <footer className="bg-dark">
-      <ul>
-        <li>
-        </li>
-      </ul>
       <div className="text-center py-5">
         <small className="text-white fw-bold">Kengo Masunari</small>
       </div>
